e6eaaf6ef3217a84811cbaec0f725cd0801927a8
Juan Raymundo Carrillo Jasso <raymundo>
2021-01-13 11:13:43 -0600

    rename email column from recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_020440) do
+ActiveRecord::Schema.define(version: 2021_01_13_165516) do
 
   create_table "chefs", force: :cascade do |t|
     t.string "chefname"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2021_01_13_020440) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
